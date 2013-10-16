733e132de755f9b4d8a8fdcc69cf1292714955f9
santhan-k <skus887@aucklanduni.ac.nz>
2013-10-16 02:48:53 -0700

    Linked home page back to the welcome screen

--- CCF2/CCF2/HomePage.xaml.cs
+++ CCF2/CCF2/HomePage.xaml.cs
@@ -56,7 +56,11 @@ namespace CCF2
         {
 
         }
-
+        //Action listener for the event that is caused when the logo is clicked
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            sw1.showPage(new WelcomeScreen(sw1));
+        }
         private void ContactUs_Click(object sender, RoutedEventArgs e)
         {
             //sw1.showPage(new testNE(sw1, "testNE"));
