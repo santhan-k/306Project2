b59dc5a5c3ceb27969df98b0af6147608cfbcb24
mtoo020 <mtoo020@aucklanduni.ac.nz>
2013-10-22 01:55:59 -0700

    News images are now pulled from the web. Need to create xml file and repeat for events.

--- CCF2/CCF2/NewsAndEvents.xaml.cs
+++ CCF2/CCF2/NewsAndEvents.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HtmlAgilityPack;
+using System.Xml.XPath;
+using System.Net;
+using System.IO;
+using System.Xml;
 
 namespace CCF2
 {
@@ -25,6 +30,51 @@ namespace CCF2
         {
             sw3 = window;
             InitializeComponent();
+            String ccfURL = "http://childcancer.org.nz";
+            String newsURL = "/News-and-events/News.aspx";
+            String imagesFolder = "Resources/images/";
+            HtmlDocument newsHTML;
+            WebClient web = new WebClient();
+            //try
+            //{
+            //    using (Stream stream = new WebClient().OpenRead(newsURL))
+            //    {
+            //        newsHTML = new HtmlWeb().Load(ccfUrl + newsURL);
+            //        newsHTML.Save(newsUrl.);
+            //        create xml file
+            //    }
+            //}
+            //catch
+            //{
+                //MessageBox.Show("Could not connect to the Child Cancer Foundation website.\n\nPreviously downloaded news and events will be shown.");
+                newsHTML = new HtmlDocument();
+                newsHTML.Load(filenameFromPath(newsURL));
+            //}
+                int i = 0;
+                foreach (HtmlNode n in newsHTML.DocumentNode.SelectNodes("//div[@id='contentPrimary']//div[@class='item']"))
+                {
+                    String title = n.SelectSingleNode("h3/a").InnerText;
+                    String articleURL = n.SelectSingleNode("h3/a").Attributes["href"].Value;
+                    String date = n.SelectSingleNode("small").InnerText;
+                    String caption = n.SelectSingleNode("p").InnerText;
+                    HtmlNode articleImage = n.SelectSingleNode("img");
+                    if (articleImage != null)
+                    {
+                        web.DownloadFile(new Uri(ccfURL + articleImage.Attributes["src"].Value), imagesFolder + i++.ToString() + ".jpg");
+                    }
+
+                    HtmlDocument articleHTML = new HtmlDocument();
+                    articleHTML.Load(filenameFromPath(articleURL));
+
+                    if (articleHTML.DocumentNode.SelectSingleNode("//div[@id='contentPrimary']//img") != null)
+                    {
+                        foreach (HtmlNode img in articleHTML.DocumentNode.SelectNodes("//div[@id='contentPrimary']//img"))
+                        {
+                            web.DownloadFile(new Uri(ccfURL + img.Attributes["src"].Value), imagesFolder + i++.ToString() + ".jpg");
+                        }
+                    }
+                    
+                }
 
             //If new page is initiated from the home page, it comes in the form the right
             if (name == "newsandevents")
@@ -38,6 +88,11 @@ namespace CCF2
                 sw3.showP = (window.Resources["SlidePageRightEntry"] as Storyboard).Clone();
             }
 
+        }
+
+        private String filenameFromPath(String path)
+        {
+            return path.Substring(path.LastIndexOf('/') + 1);
         }
 
         //Action listener for the back button
