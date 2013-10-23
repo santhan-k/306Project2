ef512a20237336eb7357f265f04372d0956632a5
gcha140 <gcha140@aucklanduni.ac.nz>
2013-10-23 16:55:50 +1300

    Family Support subpages now support touch swipe

--- CCF2/CCF2/FamilySupportSubpage.xaml.cs
+++ CCF2/CCF2/FamilySupportSubpage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
+using Microsoft.Surface.Presentation.Input;
 
 namespace CCF2
 {
@@ -48,12 +49,47 @@ namespace CCF2
 
         private void bodyImage_ImageFailed(object sender, ExceptionRoutedEventArgs e)
         {
-
+            // insert code here
         }
 
         private void Home_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new HomePage(sw1));
+        }
+
+        private Dictionary<TouchDevice, Point> currentTouchDevices = new Dictionary<TouchDevice, Point>();
+
+        private void FamilySupportSubPage_Touch_TouchDown(object sender, TouchEventArgs e)
+        {
+            currentTouchDevices.Add(e.TouchDevice, e.TouchDevice.GetPosition(this));
+        }
+
+        private void FamilySupportSubPage_Touch_TouchUp(object sender, TouchEventArgs e)
+        {
+            currentTouchDevices.Remove(e.TouchDevice);
+        }
+
+        private void FamilySupportSubPage_Touch_TouchMove(object sender, TouchEventArgs e)
+        {
+            if (currentTouchDevices.Count == 1)
+            {
+                int isRight = 0;
+
+                foreach (KeyValuePair<TouchDevice, Point> td in currentTouchDevices)
+                {
+                    if (td.Key != null && e.TouchDevice.GetPosition(this).X - td.Value.X > 100)
+                        isRight++;
+                    else
+                        return;
+                }
+
+                if (isRight == 1)
+                {
+                    sw1.showPage(new FamilySupport(sw1, "back"));
+                    currentTouchDevices.Clear();
+                    return;
+                }
+            }
         }
     }
 }
