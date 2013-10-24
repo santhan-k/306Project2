7a606e980fbe05e5fcd3f220709ab7c920f0adb5
gcha140 <gcha140@aucklanduni.ac.nz>
2013-10-24 14:50:28 +1300

    Added comments for Family Support

--- CCF2/CCF2/familysupport.xaml.cs
+++ CCF2/CCF2/familysupport.xaml.cs
@@ -26,6 +26,12 @@ namespace CCF2
         {
             sw1 = window;
             InitializeComponent();
+
+            /* As the user goes through the pages, the next page slides into focus from the right.
+             * The current page slides to the left and disappears. Vice versa, as the user goes
+             * back, the previous page slides into focus from the left and the current page slides
+             * to the right and disappears.
+             */
             if (name == "familysupport")
             {
                 sw1.hideP = (window.Resources["SlidePageLeftExit"] as Storyboard).Clone();
@@ -39,59 +45,75 @@ namespace CCF2
 
         }
 
-        //Back button listener
+        // Touching the back button will take the user to the homepage
         private void Back_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new HomePage(sw1));
         }
 
+        // Touching the SupportCo-ordinators button will take the user to the Support Co-ordinators page
         private void FSC_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new FamilySupportSubPage(sw1, "FSC"));
         }
 
+        // Touching the BeadsofCourage® button will take the user to the Beads of Courage® page
         private void BeadsOfCourage_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new FamilySupportSubPage(sw1, "BeadsOfCourage"));
         }
 
+        // Touching the SharingMagazine button will take the user to the Sharing Magazine page
         private void SharingMagazine_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new FamilySupportSubPage(sw1, "SharingMagazine"));
         }
 
+        // Touching the FamilyPlaces button will take the user to the Family Places page
         private void FamilyPlaces_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new FamilySupportSubPage(sw1, "FamilyPlaces"));
         }
 
+        // Touching the HolidayHomes button will take the user to the Holiday Homes page
         private void HolidayHomes_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new FamilySupportSubPage(sw1, "HolidayHomes"));
         }
 
+        // Touching the Scholarships button will take the user to the Scholarships page
         private void Scholarships_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new FamilySupportSubPage(sw1, "Scholarships"));
         }
 
+        // Touching the CCF logo will take the user to the homepage
         private void Home_Click(object sender, RoutedEventArgs e)
         {
             sw1.showPage(new HomePage(sw1));
         }
 
+        /* The user can go back to the previous page by swiping their finger to the right.
+         * The swipe gesture is made up of 3 phases:
+         * Initial phase: TouchDown, the moment when the user touches the screen.
+         * Middle phase: TouchMove, the speed and direction of the user as they move their finger across the screen.
+         * Final phase: TouchUp, the moment when the user's finger leaves the screen.
+         */
         private Dictionary<TouchDevice, Point> currentTouchDevices = new Dictionary<TouchDevice, Point>();
 
+        // TouchDown event triggers the moment when the user touches the screen and captures the (x,y) position of the touch
         private void FamilySupport_Touch_TouchDown(object sender, TouchEventArgs e)
         {
             currentTouchDevices.Add(e.TouchDevice, e.TouchDevice.GetPosition(this));
         }
 
+        // TouchUp event triggers the moment when the user's finger leaves the screen
         private void FamilySupport_Touch_TouchUp(object sender, TouchEventArgs e)
         {
             currentTouchDevices.Remove(e.TouchDevice);
         }
 
+        // TouchMove event triggers when the user's finger moves quickly across the screen
         private void FamilySupport_Touch_TouchMove(object sender, TouchEventArgs e)
         {
             if (currentTouchDevices.Count == 1)
